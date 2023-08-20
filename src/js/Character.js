9351b144759a81959db4784140649d50841b7787
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-08-19 20:25:34 -0400

    task done

--- src/js/Character.js
+++ src/js/Character.js
@@ -14,7 +14,26 @@ export default class Character {
 
   level = 1;
 
-  attack;
+  attack = 10;
 
-  defence;
+  defence = 10;
+
+  levelUp() {
+    if (this.health === 0) {
+      throw new Error('Character is dead now');
+    }
+    this.level += 1;
+    this.attack *= 1.2;
+    this.defence *= 1.2;
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100);
+    }
+    if (this.health < 0) {
+      this.health = 0;
+    }
+  }
 }
--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -34,6 +34,46 @@ const typesHandler = test.each(typesDatalist);
 
 typesHandler('Class Character for %s', (type) => {
   expect(new Character('Ivan', type)).toEqual({
-    attack: undefined, defence: undefined, health: 100, level: 1, name: 'Ivan', type,
+    attack: 10, defence: 10, health: 100, level: 1, name: 'Ivan', type,
+  });
+});
+
+function levelUp() {
+  const instance = new Character('Ivan', 'Bowman');
+  instance.levelUp();
+  return instance;
+}
+test('Level Up', () => {
+  expect(levelUp()).toEqual({
+    attack: 12, defence: 12, health: 100, level: 2, name: 'Ivan', type: 'Bowman',
+  });
+});
+
+function levelUpError() {
+  const instance = new Character('Ivan', 'Bowman');
+  instance.health = 0;
+  instance.levelUp();
+  return instance;
+}
+test('Level Up', () => {
+  expect(levelUpError).toThrow('Character is dead now');
+});
+
+const damages = [
+  [112, 0],
+  [50, 55],
+];
+
+const damagesHandler = test.each(damages);
+
+function damageTest(damage) {
+  const instance = new Character('Ivan', 'Bowman');
+  instance.damage(damage);
+  return instance;
+}
+
+damagesHandler('Damage test for %s', (damage, health) => {
+  expect(damageTest(damage)).toEqual({
+    attack: 10, defence: 10, health, level: 1, name: 'Ivan', type: 'Bowman',
   });
 });
